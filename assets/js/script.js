a723fa0da73e010ee4074eee704f9e5fda7b5fed
Hunter McGrew <hunter109@gmail.com>
2022-08-29 16:16:46 -0500

    trying to fix js problem on IOS

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ resumeNavLink.forEach(item => {
     item.addEventListener("click", resumeDelay);
     function resumeDelay() {
         setTimeout(() => {
-            window.open("https://huntermcgrew.github.io/Portfolio-WIP/assets/Resume.pdf");
+            window.open("https://huntermcgrew.github.io/Portfolio-WIP/assets/Resume.pdf", "_blank");
     }, 1000)
     }
 })
